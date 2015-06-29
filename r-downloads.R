3e71c7133de13b42be7959630e2adecb8830803e
Joe Cheng <joe@rstudio.com>
2015-06-29 12:36:49 +0200

    Country vs. hour

--- r-downloads.R
+++ r-downloads.R
@@ -2,10 +2,12 @@ library(dplyr)
 library(d3heatmap)
 library(dygraphs)
 library(xts)
+library(reshape2)
 
 downloads <- do.call(rbind, lapply(list.files("data", full.names = TRUE), read.csv, stringsAsFactors=FALSE))
+downloads <- downloads %>% mutate(hour=sub(":.*", "", time))
 
-counts <- downloads %>% mutate(hour=sub(":.*", "", time)) %>%
+counts <- downloads %>%
   filter(country %in% c("US")) %>%
   count(date, hour) %>%
   arrange(date, hour)
@@ -25,3 +27,14 @@ rownames(m_counts) <- unique(counts$hour)
 m_counts["08","06/22"] <- NA
 
 d3heatmap(t(m_counts), colors = "Greens", dendrogram = "none", scale = "none")
+
+major_countries <- downloads %>% count(country) %>% filter(n > 100) %>% `$`("country")
+hours_countries <- downloads %>%
+  filter(country %in% major_countries) %>%
+  count(hour, country) %>%
+  arrange(hour, country)
+m_hours_countries <- acast(hours_countries, hour ~ country, value.var = 'n')
+m_hours_countries[is.na(m_hours_countries)] <- 0
+
+d3heatmap(m_hours_countries, colors = "Blues", dendrogram = "col", scale = "col")
+d3heatmap(scale(m_hours_countries), colors = "Blues", dendrogram = "col", scale = "col")
